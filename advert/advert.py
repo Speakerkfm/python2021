a047510e936d004a6de0e950e6c3b98f8454928c
a.usanin <usanin.a@7gis.ru>
2021-04-11 15:40:27 +0500

    add advert hometask

---
+++ advert/advert.py
@@ -0,0 +1,72 @@
+import json
+import keyword
+
+
+class ColorizeMixin:
+    """
+    Print title and price attribute with yellow color
+    """
+    repr_color_code = 33
+
+    def __repr__(self):
+        return f'\033[1;{self.repr_color_code};1m ' \
+               f'{self.title} | {self.price} ' \
+               f'\033[0;1;1m'
+
+
+class Advert(ColorizeMixin):
+    """
+    Contains advert info
+    """
+    _price = 0
+
+    def __setattr__(self, key, value):
+        if keyword.iskeyword(key):
+            key = key + "_"
+        super.__setattr__(self, key, value)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, var):
+        if var < 0:
+            raise ValueError("must be >= 0")
+        self._price = var
+
+
+def obj_from_dict(data: dict, cls: object) -> object:
+    """
+    Convert dict to object
+    :param data: dict
+    :param cls: class description
+    :return: object ob cls
+    """
+    obj = cls()
+    for a, b in data.items():
+        if isinstance(b, (list, tuple)):
+            obj.__setattr__(a, [obj_from_dict(x, cls)
+                                if isinstance(x, dict)
+                                else x for x in b])
+        else:
+            obj.__setattr__(a, obj_from_dict(b, cls)
+            if isinstance(b, dict) else b)
+    return obj
+
+
+lesson_str = """{
+  "title": "Вельш-корги",
+  "price": 1000,
+  "class": "dogs",
+  "location": {
+    "address": "сельское поселение Ельдигинское, поселок санатория Тишково, 25"
+  }
+}"""
+
+if __name__ == '__main__':
+    lesson = json.loads(lesson_str)
+    corgi = obj_from_dict(lesson, Advert)
+    print(corgi.class_)
+    print(corgi)
+    print(corgi.location.address)
